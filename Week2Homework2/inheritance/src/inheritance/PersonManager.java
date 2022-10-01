e49244d98e306b6101dfe9e1fe4d068047ff7910
Alperen Saylar <alpisa1907@gmail.com>
2022-10-02 00:55:07 +0300

    Week2Homework2
    Week2Homework2

---
+++ Week2Homework2/inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class PersonManager {
+    public void List(){
+        System.out.println("Listelendi");
+    }
+
+    public void Aist(){
+        System.out.println("Eklendi");
+    }
+}
